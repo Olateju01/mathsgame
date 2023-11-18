183f2d209c615d75cb8b8f4a20b23c600fd507a9
Olateju <3legged01@gmail.com>
2023-11-18 08:52:12 +0000

    mathgame modified

--- mathgame.py
+++ mathgame.py
@@ -1,4 +1,8 @@
 Fnum = int(input("Enter first number "))
 Snum = int(input("Enter Second number "))
 NumSum = Fnum + Snum
+NumProd= Fnum * Snum
+NumDiff= Fnum - Snum
 print(f"The sum of the two numbers is ", NumSum)
+print(f"The product of the two numbers is ", NumProd)
+print(f"The difference between the two numbers is ", NumDiff)
